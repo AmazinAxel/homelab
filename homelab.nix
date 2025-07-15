7ea4b196e8380c23eba9a36ca87363b249e859a2
AmazinAxel <axel@amazinaxel.com>
2025-07-15 09:28:17 -0700

    fix firewall

--- homelab.nix
+++ homelab.nix
@@ -32,11 +32,14 @@
   };
 
   # Networking
-  networking.wireless.iwd = {
-    enable = true;
-    settings = {
-      IPv6.Enabled = true;
-      Settings.AutoConnect = true;
+  networking = {
+    firewall.allowedTCPPorts = [ 80 ];
+    wireless.iwd = {
+      enable = true;
+      settings = {
+        IPv6.Enabled = true;
+        Settings.AutoConnect = true;
+      };
     };
   };
 
