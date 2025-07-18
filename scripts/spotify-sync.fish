63a2fb2ec700bd660145b8ead0e9595c6f715bf2
AmazinAxel <axel@amazinaxel.com>
2025-07-18 09:54:35 -0700

    sync

--- scripts/spotify-sync.fish
+++ scripts/spotify-sync.fish
@@ -1,15 +1,15 @@
 #!/usr/bin/env fish
 
 # Check & get mounted drive
-set subdirs (find . -mindepth 1 -maxdepth 1 -type d)
-if test (count $subdirs) -eq 1
-    set dir $subdirs[1]
+set drives (find /media -mindepth 1 -maxdepth 1 -type d)
+if test (count $drives) -eq 1
+    set driveDir $drives[1]
 else
-    echo "Improper drive amount detected (expected one)"
+    echo "Improper drive amount detected, found "(count $drives)
     exit 1
 end
 
-mkdir "$dir/Music"
+mkdir "$driveDir/Music"
 set playlists \
     "Synthwave https://open.spotify.com/playlist/1YIe34rcmLjCYpY9wJoM2p" \
     "Focus https://open.spotify.com/playlist/3Qk9br14pjEo2aRItDhb2f" \
@@ -21,5 +21,5 @@ set playlists \
 for playlist in $playlists
     set name (echo $playlist | awk '{print $1}')
     set url (echo $playlist | awk '{print $2}')
-    sudo spotdl download "$url" --output "$dir/Music/$name"
+    sudo spotdl download "$url" --output "$driveDir/Music/$name"
 end
