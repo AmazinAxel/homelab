db0a11da2416bc6c083b6b6040cc1147ae81a85d
AmazinAxel <axel@amazinaxel.com>
2025-08-21 15:10:45 -0700

    feat: remove captureImg/hourly service

--- services.nix
+++ services.nix
@@ -24,26 +24,16 @@
           ${pkgs.udevil}/bin/devmon
         '';
       };
-      "hourly".script = ''
-        ${pkgs.fish}/bin/fish /home/alec/homelab/scripts/captureImg.fish
-      '';
       "daily".script = ''
         ${pkgs.fish}/bin/fish /home/alec/homelab/scripts/githubBackup.fish
         ${pkgs.fish}/bin/fish /home/alec/homelab/scripts/spotifySync.fish
       '';
     };
 
-    timers = {
-      "hourly" = { # Every hour, every day
-        wantedBy = [ "timers.target" ];
-        partOf = [ "captureImg.service" ];
-        timerConfig.OnCalendar = "*-*-* *:00:00";
-      };
-      "daily" = { # Every morning at 3AM PT
-        wantedBy = [ "timers.target" ];
-        partOf = [ "daily.service" ];
-        timerConfig.OnCalendar = "*-*-* 03:00:00";
-      };
+    timers."daily" = { # Every morning at 3AM PT
+      wantedBy = [ "timers.target" ];
+      partOf = [ "daily.service" ];
+      timerConfig.OnCalendar = "*-*-* 03:00:00";
     };
   };
 }
