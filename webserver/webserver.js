38d2d2f4608c48599a0cb4fa13356e0512fb6ccd
AmazinAxel <axel@amazinaxel.com>
2025-08-30 13:41:37 -0700

    fix: fix error

--- webserver/webserver.js
+++ webserver/webserver.js
@@ -84,6 +84,6 @@ try {
   });
 } catch(e) {
   console.log('A webserver service is already running - killing old one');
-  spawnSync("pkill", ["bun"]);
+  spawnSync(["pkill", "bun"]);
   process.exit(1);
 };
