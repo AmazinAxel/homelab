0b53fb438371c7efc8fb9a0d0e027796f4b85861
AmazinAxel <axel@amazinaxel.com>
2026-01-29 11:26:10 -0800

    feat: redesign overview layout

--- display/program/menus/overview.c
+++ display/program/menus/overview.c
@@ -1,21 +1,15 @@
 #include "../LCD/LCD_1in44.h"
 #include "../GUI/GUI_Paint.h"
-#include <stdio.h> // #printf
-#include <stdlib.h>	// #exit
-#include <signal.h> // #signal
-
-extern LCD_DIS sLCD_DIS;
 
 void overviewMenu(UWORD *BlackImage) {
     Paint_NewImage(BlackImage, LCD_WIDTH, LCD_HEIGHT, BACKGROUND_COLOR, 16);
     Paint_ClearWindow(0, 0, 128, 128, BACKGROUND_COLOR);
 
-    Paint_DrawLine(8, 8, 128 - 7, 8, BLACK, DOT_PIXEL_8X8, LINE_STYLE_SOLID);
+    Paint_DrawLine(8, 8, 128 - 7, 8, BLACK4, DOT_PIXEL_8X8, LINE_STYLE_SOLID);
+    Paint_DrawLine(8, 17 + 4, 128 - 7, 17 + 4, BLACK4, DOT_PIXEL_8X8, LINE_STYLE_SOLID);
+    Paint_DrawString_EN(6, 3, "homelab", &Font24, BLACK4, BLUE3);
 
-    Paint_DrawLine(8, 17 + 4, 128 - 7, 17 + 4, BLACK, DOT_PIXEL_8X8, LINE_STYLE_SOLID);
-
-    Paint_DrawString_EN(6, 3, "Homelab", &Font24, BLACK, BLUE2);
-    Paint_DrawString_EN(0, 40, "Backed up:", &Font16, BACKGROUND_COLOR, WHITE);
-    Paint_DrawString_EN(0, 65, "Network:", &Font16, BACKGROUND_COLOR, WHITE);
-    Paint_DrawString_EN(0, 90, "Storage:", &Font16, BACKGROUND_COLOR, WHITE);
+    Paint_DrawString_EN(8, 45, "Backed up:", &Font12, BACKGROUND_COLOR, FONT_COLOR);
+    Paint_DrawString_EN(8, 70, "Network:", &Font12, BACKGROUND_COLOR, FONT_COLOR);
+    Paint_DrawString_EN(8, 95, "Storage:", &Font12, BACKGROUND_COLOR, FONT_COLOR);
 };
