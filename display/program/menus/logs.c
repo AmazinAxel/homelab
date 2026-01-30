3f495343839145624c53a19be7605ca7f5e095a4
AmazinAxel <axel@amazinaxel.com>
2026-01-30 10:28:30 -0800

    fix: fix log program segfault

--- display/program/menus/logs.c
+++ display/program/menus/logs.c
@@ -1,5 +1,6 @@
 #include "../LCD/LCD_1in44.h"
 #include "../GUI/GUI_Paint.h"
+#include <string.h>
 
 void logsMenu() {
     Paint_DrawString(4, 4, "Logs", &Font20, BACKGROUND_COLOR, BLUE3);
@@ -7,13 +8,18 @@ void logsMenu() {
     FILE *logFile;
     logFile = fopen("/home/alec/logs.txt", "r");
     char line[100]; // Max line size
-    int lineY = 20;
+    int lineY = 28;
 
     while (fgets(line, sizeof line, logFile)) {
-        printf("Log entry: %s\n", line);
+
+        // Get rid of newlines or extra spaces because that segfaults the drawString function
+        size_t len = strlen(line);
+        if (len > 0 && line[len - 1] == '\n')
+            line[len - 1] = '\0';
+
         Paint_DrawString(4, lineY, line, &Font12, BACKGROUND_COLOR, FONT_COLOR);
 
-        lineY += 12;
+        lineY += 15;
     };
 
     fclose(logFile);
