d11ca0ca3a25c739a696582c69f7f05c35811a4b
AmazinAxel <axel@amazinaxel.com>
2026-04-14 14:15:38 -0700

    feat: remove nixosSystem configuration, flake used for building now

--- flake.nix
+++ flake.nix
@@ -5,21 +5,26 @@
 
   outputs = { self, nixpkgs }:
     let
-      pkgs = nixpkgs.legacyPackages.x86_64-linux;
+      pkgs = nixpkgs.legacyPackages.aarch64-linux;
     in {
-      devShells.x86_64-linux.default = nixpkgs.legacyPackages.x86_64-linux.mkShell {
-        buildInputs = with pkgs; [
-          pkgsCross.aarch64-multiplatform.buildPackages.gcc
-          pkgsCross.aarch64-multiplatform.libgpiod_1
-          lftp
-        ];
-
-        shellHook = ''
-          deploytohomelab() {
-            make -j$(nproc)
-            lftp -u alec -e "put homelabDisplay -o /home/alec/homelab/display/homelabDisplay; bye" sftp://alechomelab.local
-          }
+      packages.aarch64-linux.homelabDisplay = pkgs.stdenv.mkDerivation {
+        name = "homelabDisplay";
+        src = ./display;
+        buildInputs = [ pkgs.libgpiod_1 ];
+        preBuild = "make clean";
+        buildPhase = "make CC=$CC";
+        installPhase = ''
+          mkdir -p $out/bin
+          cp homelabDisplay $out/bin/
         '';
       };
+
+      devShells.x86_64-linux.default =
+        let x86pkgs = nixpkgs.legacyPackages.x86_64-linux; in x86pkgs.mkShell {
+          buildInputs = with x86pkgs; [
+            pkgsCross.aarch64-multiplatform.buildPackages.gcc
+            pkgsCross.aarch64-multiplatform.libgpiod_1
+          ];
+        };
     };
 }
